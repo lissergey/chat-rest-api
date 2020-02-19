8ecc334c32b196b12ff893e95e6d4d9baa6c8a4d
Sergey Lisakonov <lisakonov@Ukr.net>
2020-02-19 13:24:03 +0200

    Update index.js

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,5 +1,5 @@
 module.exports =  {
-    "port": 3005,
+    "port": process.env.PORT,
     "mongoUrl": "mongodb+srv://admin:Sergey123@cluster0-r15fh.mongodb.net/test?retryWrites=true&w=majority",
     "bodyLimit": "100kb"
 }
